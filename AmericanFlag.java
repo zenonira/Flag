fa52c7dcbe3f7d8103ca398c92ab4418fe49b21e
apcs <apcs@mvhs-u528ap03.dcsdk12.org>
2013-10-25 09:09:44 -0600

    No Stars

--- AmericanFlag.java
+++ AmericanFlag.java
@@ -24,44 +24,29 @@ public class AmericanFlag {
 
 	public void draw(Graphics g) {
 		int y = 0;
+		double newLength = width*.76;
 
-		this.blue = new Rectangle(200, 200, Color.blue, new Point(0,0));
+		this.blue = new Rectangle((int)newLength, (int)width*7/13, Color.blue, new Point(0,0));
 		this.point = new Point(0,0);
+		System.out.println(newLength);
 
 		for (int i=1; i<=stripes.length; i++) {
 			if (i%2==0) {
-				this.white = new Rectangle(length, 40, Color.white, new Point(0,y));
+				this.white = new Rectangle(length, (int)width/13, Color.white, new Point(0,y));
 				stripes[i-1] = this.white;
-				System.out.println("Print White" + y);
-				y += 40;
+				y += width/13;
 			} else {
-				this.red = new Rectangle(length, 40, Color.red, new Point(0,y));
+				this.red = new Rectangle(length, (int)width/13, Color.red, new Point(0,y));
 				stripes[i-1] = this.red;
-				System.out.println("Pring red" + y);
-				y += 40;
+				y += width/13;
 			}
 		}
 
 		for (int i = 0; i<stripes.length; i++) {
 			stripes[i].paint(g);
-			System.out.println("Done");
 		}
 
 		this.blue.paint(g);
-		// red.paint(g, 0, 0);
-		// white.paint(g, 0, 40);
-		// red.paint(g, 0, 80);
-		// white.paint(g, 0, 120);
-		// red.paint(g, 0, 160);
-		// white.paint(g, 0, 200);
-		// red.paint(g, 0, 240);
-		// white.paint(g, 0, 280);
-		// red.paint(g, 0, 320);
-		// white.paint(g, 0, 360);
-		// red.paint(g, 0, 400);
-		// white.paint(g, 0, 440);
-		// red.paint(g, 0, 480);
-		// blue.paint(g, 0, 0);
 	}
 	
 }
